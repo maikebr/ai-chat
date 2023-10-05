158b3e5db46f66e259ec50b1f54592b36b6e29ca
Maike Muzitano <maikemuzitano@gmail.com>
2023-10-04 23:12:20 -0300

    dark theme  added

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,36 @@
-'use client';
+"use client";
 
-import { useChat } from 'ai/react';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useChat } from "ai/react";
+import { PaperPlaneIcon } from "@radix-ui/react-icons";
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+	const { messages, input, handleInputChange, handleSubmit } = useChat();
 
-  return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.length > 0
-        ? messages.map(m => (
-            <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === 'user' ? 'User: ' : 'AI: '}
-              {m.content}
-            </div>
-          ))
-        : null}
+	return (
+		<div className="flex flex-col w-full max-w-2xl py-12 mx-auto border border-black h-full p-4">
+			{messages.length > 0
+				? messages.map((m) => (
+						<div key={m.id} className="whitespace-pre-wrap  text-slate-gray">
+							{m.role === "user" ? "User: " : "IA: "}
+							{m.content}
+						</div>
+				  ))
+				: null}
 
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
-    </div>
-  );
+			<form onSubmit={handleSubmit}>
+				<div className="fixed bottom-10 flex gap-2 w-full max-w-2xl">
+					<Input
+						value={input}
+						placeholder="Say something..."
+						onChange={handleInputChange}
+					/>
+					<Button type="submit" size="icon">
+						<PaperPlaneIcon />
+					</Button>
+				</div>
+			</form>
+		</div>
+	);
 }
