3504da1f00e89e674c6407a85c28e6a3d5697baf
Maike Muzitano <maikemuzitano@gmail.com>
2023-10-05 11:31:42 -0300

    minimal ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
 				<body>
 					<ThemeProvider
 						attribute="class"
-						defaultTheme="system"
+						defaultTheme="dark"
 						enableSystem
 						disableTransitionOnChange>
 						{children}
--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,39 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useChat } from "ai/react";
 import { PaperPlaneIcon } from "@radix-ui/react-icons";
+import { useRef, useEffect } from "react";
 
 export default function Chat() {
 	const { messages, input, handleInputChange, handleSubmit } = useChat();
 
 	return (
-		<div className="flex flex-col w-full max-w-2xl py-12 mx-auto border border-black h-full p-4">
-			{messages.length > 0
-				? messages.map((m) => (
-						<div key={m.id} className="whitespace-pre-wrap  text-slate-gray">
-							{m.role === "user" ? "User: " : "IA: "}
-							{m.content}
-						</div>
-				  ))
-				: null}
+		<div className="relative w-full max-w-4xl py-6 mx-auto p-4">
+			<h1 className="font-semibold dark:text-slate-400 text-slate-600 text-xl leadind-normal p-2 text-center">
+				MaikeGPT
+			</h1>
+			<div className="border-x min-h-[90vh] rounded-lg p-5">
+				{messages.length > 0
+					? messages.map((m) => (
+							<p
+								key={m.id}
+								className={`whitespace-pre-wrap p-4 mb-4 rounded-xl
+                            ${
+															m.role === "user"
+																? "text-left  bg-slate-900 "
+																: "text-left bg-slate-800"
+														}
+                `}>
+								{m.content}
+							</p>
+					  ))
+					: null}
+			</div>
 
 			<form onSubmit={handleSubmit}>
-				<div className="fixed bottom-10 flex gap-2 w-full max-w-2xl">
+				<div className="absolute inset-x-0 bottom-0 mx-14 flex gap-5  w-full max-w-3xl">
 					<Input
 						value={input}
-						placeholder="Say something..."
+						placeholder="What do you want to talk about..."
 						onChange={handleInputChange}
 					/>
 					<Button type="submit" size="icon">
